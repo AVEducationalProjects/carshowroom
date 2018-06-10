3a8d63f50745d72c8511934494a962cd015bfcaf
Alexander Morozov <amorozov@adeptik.com>
2018-06-10 15:52:01 +0400

    Vendoe editing

--- src/Db/CRMContext.cs
+++ src/Db/CRMContext.cs
@@ -11,10 +11,16 @@ namespace CarShowRoom.Db
 
         public DbSet<Service> Services { get; set; }
 
+        public DbSet<Vendor> Vendors { get; set; }
+
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Service>().Property(x=>x.Name).IsRequired();
             modelBuilder.Entity<Service>().Property(x=>x.Price).IsRequired();
+
+            modelBuilder.Entity<Vendor>().Property(x => x.Name).IsRequired();
         }
     }
 }
---
+++ src/Migrations/20180610113853_Vendors.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CarShowRoom.Migrations
+{
+    public partial class Vendors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vendors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendors", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vendors");
+        }
+    }
+}
